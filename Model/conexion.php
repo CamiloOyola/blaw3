545d3d05ae978b1f21adcae910bf0fd49da4cd1c
CamiloOyola <100384120+CamiloOyola@users.noreply.github.com>
2023-06-16 17:47:52 -0500

    Update conexion.php

--- Model/conexion.php
+++ Model/conexion.php
@@ -24,36 +24,4 @@ function conexion()
 $objConexion = conexion();
 
 
-
-
-
-
-
-/* Conexion inicial
-
-//conectar Mysql
-$conexion = mysqli_connect("localhost", "root", "admin", "blaw");
-
-//Probar conexion
-if (mysqli_connect_errno()) {
-    echo "Fallo al conectar a MySQL: (" . mysqli_connect_error . ") " . mysqli_connect_error();
-}else{
-    echo "conexion successful";
-}
-
----------------------
-<?php
-$host = 'localhost'; //  servidor de base de datos 
-$db = 'blaw'; // nombre de tu base de datos
-$user = 'root'; // usuario de MySQL
-$password = 'admin'; // contraseña de MySQL
-
-$conexion = new mysqli($host, $user, $password, $db);
-
-if ($conexion->connect_error) {
-    die('Error de conexión: ' . $conexion->connect_error);
-}
-}*/
-
-
 ?>
